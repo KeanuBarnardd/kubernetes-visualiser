2b917ba5adaa44d8e443a6e44c5258e0d9276796
keanubarnard <keanu.barnardd@gmail.com>
2026-06-22 20:58:31 +1000

    Added Mud UI to project

--- app/obj/Debug/net10.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net10.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa92d9faea9ef70a2ebb73b18ad937f61e18e935")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
