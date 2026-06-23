64e85f4cc3d48bfcc8abec8735dad71f7d9c7e18
keanubarnard <keanu.barnardd@gmail.com>
2026-06-24 08:27:08 +1000

    Add dark mode toggle and basic layout

--- frontend/src/lib/components/navbar.svelte
+++ frontend/src/lib/components/navbar.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-
+  import { mode } from "mode-watcher";
+  import { Button } from "$lib/components/ui/button";
+  import { toggleMode } from "mode-watcher";
 </script>
 
-<div class="flex flex-col">
-    
-</div>
+<nav class="flex flex-col gap-4 border w-minp-5 bg-card dark:bg-card-dark">
+  <div class="flex gap-4 justify-between items-center">
+    <h1 class="font-bold">Kubernetes Visualiser</h1>
+
+    <Button variant="default" onclick={toggleMode}>
+      {#if mode.current === "dark"}
+        Light
+      {:else}
+        Dark
+      {/if}
+    </Button>
+  </div>
+</nav>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
-import "./layout.css";
-import favicon from "$lib/assets/favicon.svg";
-import Button from "$lib/components/ui/button/button.svelte";
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Navbar from "$lib/components/navbar.svelte";
+  import { ModeWatcher } from "mode-watcher";
 
-let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-
-<div class="flex flex-col min-h-screen ">
-	<nav>
-		Hello this is the docs
-		<Button>Hello this is a button</Button>
-	</nav>
-	<main class="flex-1">
-		{@render children()}
-	</main>
+<div class="flex gap-5 min-h-screen bg-background p-5">
+  <ModeWatcher />
+  <Navbar />
+  <main class="flex-1">
+    {@render children()}
+  </main>
 </div>
