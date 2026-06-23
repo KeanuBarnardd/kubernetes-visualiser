be18bb9558c086d60598288ed35fc94b81e7a881
keanubarnard <keanu.barnardd@gmail.com>
2026-06-24 08:03:34 +1000

    Misc project

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+import "./layout.css";
+import favicon from "$lib/assets/favicon.svg";
+import Button from "$lib/components/ui/button/button.svelte";
 
-	let { children } = $props();
+let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+
+<div class="flex flex-col min-h-screen ">
+	<nav>
+		Hello this is the docs
+		<Button>Hello this is a button</Button>
+	</nav>
+	<main class="flex-1">
+		{@render children()}
+	</main>
+</div>
