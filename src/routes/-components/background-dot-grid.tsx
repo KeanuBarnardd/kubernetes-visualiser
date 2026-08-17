4f6ec87f6850a9070aaac4848402634517454b93
keanubarnard <keanu.barnardd@gmail.com>
2026-08-17 12:35:56 +1000

    Add our background dot

---
+++ src/routes/-components/background-dot-grid.tsx
@@ -0,0 +1,7 @@
+// Think we need to find the full page width and height then create circle divs to equally space out between them to fill the page
+
+export default function BackgroundDotGrid() {
+	return (
+		<div className="h-full w-full bg-[radial-gradient(circle,#e4e4e7_3px,transparent_3px)] bg-[length:40px_40px]" />
+	);
+}
